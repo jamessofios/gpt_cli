a7d2d60f24f88fdd0e3bb2415b292afd8849a4d3
James Sofios <jamessofios@gmail.com>
2023-08-14 17:22:20 -0500

    Removed redundant setting of tempurature to 1 in new_chatgpt(). Fixed the type of temperature (was int before, but should have been float per the openai api).

--- src/parse_options.h
+++ src/parse_options.h
@@ -1,15 +1,14 @@
 #pragma once
+#include "main.h"
+#include "construct_json.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
-
-struct main_state {
-	char *user_prompt;
-	char *system_prompt;
-	char *json_file;
-};
+#include <errno.h>
 
 void *alloc_main_state(void);
 void free_main_state(struct main_state *restrict ms);
+
+// Check the value of errno after calling this function. The value should be zero unless an error occurs.
 void parse_options(const int argc, char **restrict argv, struct main_state *restrict ms);
